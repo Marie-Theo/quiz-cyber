fa83a31e574020af65a41488792fb1a8bbe36df2
MARIE Théo <theomarie50350@gmail.com>
2026-03-14 21:05:56 +0100

    correction d'un bug pour le serveur debian

--- loginAndRegister/ConnexionBDD.php
+++ loginAndRegister/ConnexionBDD.php
@@ -21,7 +21,7 @@ class ConnexionBDD {
         $this->usermdp = $_ENV['MDP_DATABASES'];
 
         try {
-            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->usermdp);
+            $this->conn = new PDO("mysql:host=".$this->host.";dbname=".$this->dbname, $this->username, $this->usermdp);
             // set the PDO error mode to exception
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
